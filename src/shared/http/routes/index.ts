b970877af5202366023462a3b6f928346dd5d2b9
andreluizsantoss <andre.luiz.itape@gmail.com>
2023-09-02 18:59:19 -0300

    Correção dos Nomes dos Arquivos de Rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import productsRouter from "@modules/products/routes/ProductsRoutes";
-import usersRouter from "@modules/users/routes/users.routes";
-import sessionsRouter from "@modules/users/routes/sessions.routes";
-import passwordRouter from "@modules/users/routes/password.routes";
-import profileRouter from "@modules/users/routes/profile.routes";
+import usersRouter from "@modules/users/routes/UsersRoutes";
+import sessionsRouter from "@modules/users/routes/SessionsRoutes";
+import passwordRouter from "@modules/users/routes/PasswordRoutes";
+import profileRouter from "@modules/users/routes/ProfileRoutes";
 
 const routes = Router();
 
