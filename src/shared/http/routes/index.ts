b921a05acf3bd7d1edcbc7e84a980fd5de325752
andreluizsantoss <andre.luiz.itape@gmail.com>
2023-09-02 12:24:22 -0300

    Forgout Password

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import productsRouter from "@modules/products/routes/ProductsRoutes";
 import usersRouter from "@modules/users/routes/UsersRoutes";
 import sessionsRouter from "@modules/users/routes/SessionsRoutes";
+import passwordRouter from "@modules/users/routes/PasswordRoutes";
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 routes.use('/products', productsRouter);
 
 export default routes;
---
+++ src/shared/typeorm/migrations/1693661615199-CreateUserToken.ts
@@ -0,0 +1,55 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUserToken1693661615199 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'token',
+            type: 'varchar',
+          },
+          {
+            name: 'user_id',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'datetime',
+            default: 'now()',
+            isNullable: true,
+          },
+          {
+            name: 'updated_at',
+            type: 'datetime',
+            default: 'now()',
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TokenUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('user_tokens');
+    }
+
+}
