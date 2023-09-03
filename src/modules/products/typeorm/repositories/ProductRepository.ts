d632f0b3b8f64a6f9515fef224e9374ce38c5949
andreluizsantoss <andre.luiz.itape@gmail.com>
2023-09-03 19:19:42 -0300

    Relacionamentos com o TypeORM

--- src/modules/products/typeorm/repositories/ProductRepository.ts
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -1,5 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '../entities/product';
+
+interface IFindProducts {
+  id:string;
+}
 
 @EntityRepository(Product)
 class ProductRepository extends Repository<Product> {
@@ -9,8 +13,17 @@ class ProductRepository extends Repository<Product> {
         name,
       },
     });
-
     return product;
+  }
+
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    const productIds = products.map(product => product.id);
+    const existsProducts = await this.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+    return existsProducts;
   }
 }
 
---
+++ src/shared/typeorm/migrations/1693750136006-CreateOrders.ts
@@ -0,0 +1,37 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateOrders1693750136006 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'orders',
+          columns: [
+            {
+              name: 'id',
+              type: 'int',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment',
+            },
+            {
+              name: 'created_at',
+              type: 'datetime',
+              default: 'now()',
+              isNullable: true,
+            },
+            {
+              name: 'updated_at',
+              type: 'datetime',
+              default: 'now()',
+              isNullable: true,
+            },
+          ],
+        }),
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('orders');
+    }
+
+}
