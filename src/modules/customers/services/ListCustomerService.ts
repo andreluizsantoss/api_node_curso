69c9c271206b7c0231f28d5dccb8cc2eb5b55dff
andreluizsantoss <andre.luiz.itape@gmail.com>
2023-09-04 20:02:39 -0300

    Pagination

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,11 +2,22 @@ import { getCustomRepository } from "typeorm";
 import Customer from "../typeorm/entities/Customer";
 import CustomersRepository from "../typeorm/repositories/CustomerRepository";
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 class ListCustomerService {
-  public async execute() : Promise<Customer[]> {
+  public async execute() : Promise<IPaginateCustomer> {
     const customerRepository = getCustomRepository(CustomersRepository);
-    const costumers = await customerRepository.find();
-    return costumers;
+    const customers = await customerRepository.createQueryBuilder().paginate();
+    return customers as IPaginateCustomer;
   }
 }
 
